bbf4ff6ada4924f08c8a190d92d5fc6186039142
Erkan <12980@coderacademy.edu.au>
2022-10-20 15:15:39 +1100

    added models and controllers

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from controllers.cards_controller import cards_bp
+from db import db, ma
 import os
 
-from flask_sqlalchemy import SQLAlchemy
+
+# db = SQLAlchemy()
+# ma = Marshmallow()
 
 def create_app():
     app = Flask(__name__)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config ['JSON_SORT_KEYS'] = False
 
-    db = SQLAlchemy(app)
+    db.init_app(app)
+    ma.init_app(app)
 
-    @app.route('/')
-    def index():
-        return 'Hello'
+    app.register_blueprint(cards_bp)
 
     return app
